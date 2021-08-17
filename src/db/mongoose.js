1baf86b2859ccd9702f62b248535a47eb314d4f5
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-08-17 16:01:03 +0200

    fix dotenv import

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect(process.env.DB_HOST, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
